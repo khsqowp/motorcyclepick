242c9ce208642ff497e1ba7b0593758d6db295b3
khsqowp <59254269+khsqowp@users.noreply.github.com>
2025-01-30 22:17:11 +0900

    gradle error

---
+++ src/main/java/com/example/motorcyclepick/exception/DataValidationException.java
@@ -0,0 +1,11 @@
+package com.example.motorcyclepick.exception;
+
+public class DataValidationException extends RuntimeException {
+    public DataValidationException(String message) {
+        super(message);
+    }
+
+    public DataValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
